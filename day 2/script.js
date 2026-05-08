a01c0e38839d2ca694abec5edf07527b0c0d4d3e
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-08 12:39:05 +0530

    revision day 2 --- > global excution context and hoisting

---
+++ day 2/script.js
@@ -0,0 +1,84 @@
+// sum();
+
+// let a = 5;
+
+// a();
+
+
+
+// function sum() {
+//     console.log(b);
+    
+//     let b =10;
+//   console.log(a);
+// }
+
+// sum()
+
+
+
+//  ARROW FUNCTIONS
+ 
+// sum() // refernce error  becuase it is seen as undefined no information known such if it is function or number etc ( primitive or non primitive)
+
+
+// let sum = ()=>{
+//     console.log("hello");
+    
+// }
+// // sum()
+
+// console.log("go");
+
+
+
+// QUESTION
+
+// let a =10;
+
+// function outer(params) {
+//     let b = 10;
+//     console.log(a);
+//     function inner() {
+//         console.log(b);           
+//     }
+//     inner()
+    // //outer() recursion 
+// }
+
+// outer()
+
+/// ------------------------------------
+
+// sum()
+
+// var sum = ()=>{
+//     console.log("hello");
+    
+// }
+
+// sum()
+
+// let sum = ()=>{
+//     console.log("hello");
+    
+// }
+
+
+
+
+// ---------------
+
+// HOISTING questions
+
+// console.log(a);
+// var a =5;
+
+// console.log(b);
+// let b = 10;
+
+// let c;
+// console.log(c);
+// c = 10;
+
+
