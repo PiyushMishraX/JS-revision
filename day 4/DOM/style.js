e76330d7c5bc89d55b849ae6b4a108a892a7974b
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-11 15:56:18 +0530

    dom manipulataion more

--- day 4/DOM/style.js
+++ day 4/DOM/style.js
@@ -1,10 +1,57 @@
+// let btn = document.querySelector("button")
+// let body = document.querySelector("body")
+
+// let isActive = true
+// btn.addEventListener("click", function() {
+//     // console.log("hello");
+//     // body.style.backgroundColor= "black";
+
+//     if(isActive === true ){
+//         body.style.backgroundColor = "pink"
+//         isActive = false;
+//     } else {
+//         body.style.backgroundColor = "green"
+//         isActive = true;
+//     }
+// })
+
+// btn.addEventListener("click", function() {
+//     // console.log("hello");
+//     // body.style.backgroundColor= "black";
+//     body.classList.toggle('toggle')
+// })
+
+// let body = document.querySelector("body");
+// let btn = document.querySelector("button");
+
+// let h2 = document.querySelector("h2");
+
+// btn.addEventListener("click", function () {
+//     let data = prompt("enter user name");
+//     h2.innerText = "hello " + data
+//     h2.style.textAlign = "center"
+//     body.appendChild(h2);
+// })
 
 
-let btn = document.querySelector("button")
-let body = document.querySelector("body")
+// let data = prompt("enter user name");
+// h2.innerText = "HELLO " + data;
+// h2.style.textAlign = "center";
+// body.appendChild(h2);
 
-btn.addEventListener("click", function() {
-    // console.log("hello");
-    body.style.backgroundColor= "black"
-    
+
+
+
+let body = document.querySelector("body");
+let btn = document.getElementsByClassName("two"); // returns an array of all element with class two not one element so we have use indices to indicate click on specific elements
+
+// let h2 = document.querySelector("h2");
+let h2 = document.getElementById("one")
+
+
+btn[0].addEventListener("click", function () {
+    let data = prompt("enter user name");
+    h2.innerText = "hello " + data
+    h2.style.textAlign = "center"
+    body.appendChild(h2);
 })
