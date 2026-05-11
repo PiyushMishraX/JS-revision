20a92cdf764d2707cf70dddd6d3ceb26a5a5de46
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-11 13:34:18 +0530

    day 5 - scopes

---
+++ day 4/script.js
@@ -0,0 +1,30 @@
+
+// var g = 20; // global scopeed
+
+// var a = 20;
+// {
+// var a = 10;
+// }
+// console.log(a);
+
+
+// block scoped
+// let b = 20;
+// {
+//     let b = 10;
+// }
+// console.log(b);
+
+
+
+// function scope
+// function sum() {
+//     var c = 10;
+//     console.log(c);
+    
+    
+// }
+// console.log(c);
+// sum()
+
+
