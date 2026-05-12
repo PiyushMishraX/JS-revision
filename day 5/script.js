d294084b05d8b1999c754b9e2053b9cb823c5601
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-12 13:35:46 +0530

    input event listener

--- day 5/script.js
+++ day 5/script.js
@@ -1,5 +1,30 @@
-let h1 = document.querySelector("h1")
-console.log(h1.innerText);
+// let h1 = document.querySelector("h1")
+// console.log(h1.innerText);
 
-h1.innerText = "buy"
-console.log(h1.innerText);
+// h1.innerText = "buy"
+// console.log(h1.innerText);
+
+
+// let p = document.createElement("p")
+// p.innerHTML = "hello"
+
+// h1.appendChild(p) // for one element
+// // h1.append() for multiple childs at once
+
+// console.log(h1.innerHTML);
+
+// h1.remove()
+
+// console.log(h1.innerHTML);
+
+
+
+let inp = document.querySelector("input")
+let h1 = document.querySelector("h1")
+
+
+inp.addEventListener("input", (e)=>{
+    // console.log(e.target.value);
+    h1.innerText = e.target.value
+    
+})
