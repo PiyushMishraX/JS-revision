d010d20d08bf703bbf49030741cba4eedbf8e4fb
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-12 19:42:51 +0530

    keydown event listener

--- day 5/game/script.js
+++ day 5/game/script.js
@@ -1,39 +1,62 @@
-let canvas = document.querySelector("canvas")
-let pen = canvas.getContext("2d")
+let canvas = document.querySelector("canvas");
+let pen = canvas.getContext("2d");
 // pen.fillStyle = "red"
 
-// pen.fillRect(100,100,50,50) 
+// pen.fillRect(100,100,50,50)
 
-let snakeCell = [[0,0]]
+let snakeCell = [[0, 0]];
 // let snakeCell = [[0,0], [50,0]]
 
-let cell = 50
+let cell = 50;
+let direction = "right";
+
+document.addEventListener("keydown", (e) => {
+  console.log(e);
+  if (e.key == "ArrowUp") {
+    direction = "up";
+  } else if (e.key == "ArrowDown") {
+    direction = "down";
+  } else if (e.key == "ArrowLeft") {
+    direction = "left";
+  } else {
+    direction = "right";
+  }
+});
 
 function draw() {
-
-    pen.clearRect(0,0,canvas.width,canvas.height)
-    pen.fillStyle = "red"
-    for(let a of snakeCell){
-        pen.fillRect(a[0], a[1],cell,cell)
-    }
+  pen.clearRect(0, 0, canvas.width, canvas.height);
+  pen.fillStyle = "red";
+  for (let a of snakeCell) {
+    pen.fillRect(a[0], a[1], cell, cell);
+  }
 }
-draw()
+draw();
 
-function update(){
-    let headX = snakeCell[snakeCell.length-1][0] // last element X
-    let headY = snakeCell[snakeCell.length-1][1] // last element Y 
+function update() {
+  let headX = snakeCell[snakeCell.length - 1][0];
+  let headY = snakeCell[snakeCell.length - 1][1];
 
-    let newX = headX + cell 
-    let newY = headY 
-    snakeCell.push([newX,newY])
-
-    snakeCell.shift()
-    
-    // canvas.width = canvas.width
+  let newX;
+  let newY;
+  if (direction == "right") {
+    newX = headX + cell;
+    newY = headY;
+  } else if (direction == "left") {
+    newX = headX - cell;
+    newY = headY;
+  } else if (direction == "down") {
+    newX = headX;
+    newY = headY + cell;
+  } else {
+    newX = headX;
+    newY = headY - cell;
+  }
+  snakeCell.push([newX, newY]);
+  snakeCell.shift();
+  // canvas.width = canvas.width
 }
-
 
 setInterval(() => {
-    draw()
-    update()
-}, 100);
+  draw();
+  update();
+}, 200);
