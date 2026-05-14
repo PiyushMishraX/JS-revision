98b8df55520ffb7b427f2aa07c33d12de74b9b15
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-14 14:10:16 +0530

    web api,, callstack , callBack queue , event loop

---
+++ day 7/async/script.js
@@ -0,0 +1,24 @@
+setTimeout(()=>{
+    console.log("C");
+    
+},10)
+
+
+setTimeout(()=>{
+    console.log("B");
+    
+},0)
+
+console.log("A");
+
+// A, B, C --> synchronus runs immedietly while B and C goes for web api which takes ome time ,, A get stored in callStack and execute
+//  B and C enters web api , get there functionality and enters call ba k queue according to their time ( ascending order )
+
+//  the event loop ( security guard) check if callstack is empty or not ( waits longer according to bigger functions such as loop etc)
+// when it call stack is empty then sends call back queue element one by one to callstack
+
+// 
+
+
+// Web APIs 
+// log , settimeout, interval , fetch etc are provided by browser web apis which gives time loading fcntions nality 
