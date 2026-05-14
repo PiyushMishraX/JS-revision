de59fea931f153922e6f8c86fc61aac939ad25d2
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-14 13:40:18 +0530

    snake game complete

--- day 5/game/script.js
+++ day 5/game/script.js
@@ -1,14 +1,15 @@
 let canvas = document.querySelector("canvas");
 let pen = canvas.getContext("2d");
-// pen.fillStyle = "red"
-
-// pen.fillRect(100,100,50,50)
-
 let snakeCell = [[0, 0]];
-// let snakeCell = [[0,0], [50,0]]
-
 let cell = 50;
 let direction = "right";
+let gameOver = false;
+let randomCell = generateR();
+let score = 0;
+let id = setInterval(() => {
+  draw();
+  update();
+}, 200);
 
 document.addEventListener("keydown", (e) => {
   console.log(e);
@@ -24,13 +25,25 @@ document.addEventListener("keydown", (e) => {
 });
 
 function draw() {
-  pen.clearRect(0, 0, canvas.width, canvas.height);
+  if (gameOver == true) {
+    clearInterval(id);
+    pen.fillStyle = "snow";
+    pen.font = "40px sans-sarif";
+    pen.fillText("Game over!!", 200, 100);
+    return;
+  }
+
   pen.fillStyle = "red";
+  pen.clearRect(0, 0, 1000, 600);
   for (let a of snakeCell) {
     pen.fillRect(a[0], a[1], cell, cell);
   }
+  pen.fillStyle = "pink";
+  pen.font = "40px sans-sarif";
+  pen.fillText(`${score}`, 100, 300);
+  pen.fillStyle = "yellow";
+  pen.fillRect(randomCell[0], randomCell[1], cell, cell);
 }
-draw();
 
 function update() {
   let headX = snakeCell[snakeCell.length - 1][0];
@@ -41,22 +54,43 @@ function update() {
   if (direction == "right") {
     newX = headX + cell;
     newY = headY;
+    if (newX == 1000) {
+      gameOver = true;
+    }
   } else if (direction == "left") {
     newX = headX - cell;
     newY = headY;
+    if (newX < 0) {
+      gameOver = true;
+    }
   } else if (direction == "down") {
     newX = headX;
     newY = headY + cell;
+    if (newY == 600) {
+      gameOver = true;
+    }
   } else {
     newX = headX;
     newY = headY - cell;
+    if (newY < 0) {
+      gameOver = true;
+    }
   }
   snakeCell.push([newX, newY]);
-  snakeCell.shift();
-  // canvas.width = canvas.width
+  if (newX == randomCell[0] && newY == randomCell[1]) {
+    randomCell = generateR();
+    score++;
+  } else {
+    snakeCell.shift();
+  }
 }
 
-setInterval(() => {
-  draw();
-  update();
-}, 200);
+function generateR() {
+  return [
+    Math.floor((Math.random() * 950) / cell) * cell,
+    Math.floor((Math.random() * 550) / cell) * cell,
+  ];
+}
+
+// console.log(Math.random()*950,"RANDOM DATAAAA X");
+// console.log(Math.random()*550,"RANDOM DATAAAA   Y");
