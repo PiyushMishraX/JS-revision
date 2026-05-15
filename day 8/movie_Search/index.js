c574861512e5be7c1771246a33168db5bde17435
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-15 11:36:51 +0530

    use try and catch to prevent error cases

--- day 8/movie_Search/index.js
+++ day 8/movie_Search/index.js
@@ -31,16 +31,27 @@ function show(val) {
     console.log(val);
     
     list.innerHTML = ""
-    
   for (const i in val) {
 
 
+    // let img = document.createElement("img");
+    // img.setAttribute("src", val[i].show.image.original);
+
+    // list.appendChild(img);
+
+    // console.log(val[i].show.image.original);
+
     let img = document.createElement("img");
-    img.setAttribute("src", val[i].show.image.original);
 
+    try {
+      img.setAttribute("src", val[i].show.image.original);
+
+      console.log(val[i].show.image.original);
+    } catch (error) {
+        img.setAttribute("src", "https://thumbs.dreamstime.com/b/page-not-found-error-hand-drawn-ghost-doodle-vector-illustration-internet-connection-trouble-concept-105206287.jpg");
+        
+        console.log("no inp");
+    }
     list.appendChild(img);
-
-    console.log(val[i].show.image.original);
-
   }
 }
