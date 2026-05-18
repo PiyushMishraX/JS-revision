b36def38098029bd1a76d25b37f178bf1e411718
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-18 13:43:36 +0530

    day 9: Local Storage

---
+++ day 9/index.js
@@ -0,0 +1,6 @@
+// localStorage.setItem("user", "rahul")
+// console.log(localStorage.getItem("user"));
+// localStorage.removeItem("user")
+// console.log(localStorage.getItem("user"));
+
+// localStorage.clear()
