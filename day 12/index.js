403b38e1daee46c8aeb65683a35b3cc324e7e493
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-21 10:19:59 +0530

    multiple exports

--- day 12/index.js
+++ day 12/index.js
@@ -1,4 +1,23 @@
-import data from './new.js'
-import {b} from './new.js'
+// import data from './new.js'
+// import {b} from './new.js'
 
-console.log(data , b);
+// console.log(data , b);
+
+
+// import {data, sum} from './new.js'
+// import hello from './new.js'
+// import {sum} from './new.js'
+// sum(5,23)
+
+
+// console.log(data);
+
+
+console.log(hello.data);
+hello.sum(3,5)
+
+console.log(data);
+sum(3,5)
+
+// console.log(hello.a);
+// hello.b(3,5)
--- day 12/new.js
+++ day 12/new.js
@@ -1,5 +1,20 @@
 
-let a = 15;
-export default a;
+// let a = 15;
+// export default a;
 
-export let  b = 15;
+// export let  b = 15;
+
+
+let data = 15;
+function sum( a, b) {
+
+    console.log(a+b);
+    
+    
+    
+    
+}
+
+export default {data, sum}
+
+// export {data, sum}
