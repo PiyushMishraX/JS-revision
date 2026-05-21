83cf26560cc0ce335507962b88091855a7e4f3f4
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-21 11:00:24 +0530

    Currying and recursion

--- day 12/misc/index.js
+++ day 12/misc/index.js
@@ -1,25 +1,58 @@
 // ---------------- CURRYING -----------------
 
-function sum(b) {
-    return function (a) {
-        return a+b;
+// function sum(b) {
+//     return function (a) {
+//         return a+b;
+//     }
+// }
+
+// // added in ecma script
+// console.log(sum(6)(5));
+
+// function sum(c) {
+//   return function (b) {
+//     return function (a) {
+//       return a + b + c;
+//     };
+//   };
+// }
+
+// // console.log(sum(6)(5));
+// console.log(sum(6)(5)(3));
+
+// function sum(c) {
+//   return function (b) {
+//     return function (a) {
+//       return a + b + c;
+//     };
+//   };
+// }
+
+
+
+
+// function sum(a) {
+    
+//     return function (b) {
+//       if(b){
+//         return sum(a+b)
+//       }
+//       return a
+//     }
+// }
+
+function sum(a) {
+    
+    return function (b) {
+      if(b){
+        return sum(a+b)
+      }
+      return a
     }
 }
 
-// added in ecma script
-console.log(sum(6)(5));
-
-function sum(c) {
-  return function (b) {
-    return function (a) {
-      return a + b + c;
-    };
-  };
-}
-
-// console.log(sum(6)(5));
-console.log(sum(6)(5)(3));
-
+// console.log(sum(6)(5)(3)(6)(5)(3)(6)(5)(3)(6)(5)(3)());
+console.log(sum(6)(5)(3)(6)(5)(3)(6)(5)(3)(6)(5)(3));
 
 
 
