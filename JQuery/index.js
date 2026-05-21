e09149f1c4cae2c26e3918be642f59f8a3b08df5
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-21 12:55:36 +0530

    fade in , fade out using jquery

--- JQuery/index.js
+++ JQuery/index.js
@@ -1,16 +1,38 @@
-console.log($); // $ is same as document
+// console.log($); // $ is same as document
 
-let h1 = document.querySelector("h1")
-// console.log(h1.innerText);
+// let h1 = document.querySelector("h1")
+// // console.log(h1.innerText);
 
-// console.log($("h1").text());
+// // console.log($("h1").text());
 
-h1.innerText = "Hey"
-console.log(h1.innerText);
+// // h1.innerText = "Hey"
+// // console.log(h1.innerText);
 
 
-// console.log($("h1").text("No"));
-$("h1").text("No")
-console.log($("h1").text());
+// // console.log($("h1").text("No"));
+// // $("h1").text("No")
+// // console.log($("h1").text());
 
+
+// // $("h1").css("color", "red")
+// $("h1").css('color', 'red')
+
+
+$("#fadein").click(()=>{
+    console.log("fadein");
+
+    $("#container").fadeIn()
+})
+
+$("#fadeOut").click(()=>{
+    console.log("fadeOut");
+
+    $("#container").fadeOut()
+})
+
+$("#fadetoggle").click(()=>{
+    console.log("fadeOut");
+
+    $("#container").fadeToggle()
+})
 
--- day 1/script.js
+++ day 1/script.js
@@ -11,3 +11,8 @@ let arr = [1,2,3,true, "String"]
 for (const a of arr) {
     
 }
+
+
+
+// Note on Backticks (`)Unlike ' and ", backticks (template literals) are functionally different. They allow for:
+// Multi-line strings: No need for \n.String Interpolation: Using ${variable} directly inside the string.
